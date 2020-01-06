c11d2ba6b74fc5a95c2e38137c8aa6e63c201c8e
Jeff Tab <j.tab.jr@gmail.com>
2020-01-05 23:20:19 -0500

    playing with local storage

--- assets/script.js
+++ assets/script.js
@@ -2,38 +2,30 @@ var $currentDate = $("#currentDay");
 var $saveBtn = $(".saveBtn");
 var $hour = $(".hour");
 var currentHour = moment().format("H");
-console.log(currentHour);
-
 const hourCheck = document.getElementsByTagName("textarea");
+
+var textArea9 = $("#9");
+textArea9.text(localStorage.getItem("9"));
 
 $currentDate.text(moment().format("dddd, MMMM Do YYYY"));
 
-var timeCheck;
-
-
-
-
-// if ($("#21") == currentHour) {
-//     console.log("hi");
-// };
-
 for (i = 0; i < hourCheck.length; i++) {
 
-    console.log(hourCheck[i].id)
-    console.log(currentHour);
-
     if (hourCheck[i].id == currentHour) {
-        hourCheck[i].removeAttribute("class");
         hourCheck[i].setAttribute("class", "col-10 textarea present");
     };
 
     if (parseInt(hourCheck[i].id) > parseInt(currentHour)) {
-        hourCheck[i].removeAttribute("class");
         hourCheck[i].setAttribute("class", "col-10 textarea future");
+    };
+
+    if (parseInt(hourCheck[i].id) < parseInt(currentHour)) {
+        hourCheck[i].setAttribute("class", "col-10 textarea past");
     };
 };
 
 
 $(".btn").on("click", function () {
     console.log("poop");
+    localStorage.setItem("9", 9);
 });
